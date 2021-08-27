ea4bbfb4f8441e053a926587a6fb147370c9658c
TakutoYoshizumi <y.takuto.jp@gmail.com>
2021-08-27 15:16:04 +0900

    update

--- login_check.php
+++ login_check.php
@@ -1,6 +1,8 @@
 <?php
   //(C)
   require_once 'models/User.php';
+  require_once 'models/Profile.php';
+
   session_start();
   //入力値取得　POSTグローバル変数
   // var_dump($_POST);
@@ -13,14 +15,27 @@
    $user = User::login($userID, $password);
   // var_dump($user);
 
-
   //ユーザーが登録されていれば
   if ($user !== false) {
+      $id = $user->id;
+      // var_dump($id);
       $_SESSION['login_user'] = $user;
-      header('Location:top.php');
+      //ログインユーザーのプロフィールを取得
+      $profiles = Profile::find($id);
+      // var_dump($profiles);
+      // var_dump(empty($profiles));
+
+  //プロフィールに未回答の項目があれば
+      if ((empty($profiles)) === true) {
+          header('Location:profile_create.php');
+          exit;
+      } else {
+          header('Location:top.php');
+          exit;
+      }
     //条件分岐でプロフィール完成していたらトップページへ
-    // header("Location:profile_show.php");
-    exit;
+    header('Location:profile_show.php');
+      exit;
   } else {
       $errors = [];  //= $errors=array();
     $errors[] = 'お探しのユーザーが見つかりません';
